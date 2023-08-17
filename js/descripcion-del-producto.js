60086cf50759e1b9f337051cdf274cb2a0867ea4
Axel <adalvarez@scl.edu.gt>
2023-08-17 08:04:09 -0600

    info modal1

--- js/descripcion-del-producto.js
+++ js/descripcion-del-producto.js
@@ -9,15 +9,22 @@ export function caja_descripcion(data, main) {
           </section>
       
           <section class="middle">
-            <h1 class="titulo_principal">${data[main].titulo}</h1>
+            <h1 class="titulo_descri">${data[main].titulo}</h1>
             <h2 class="descripciones">${data[main].descripcion}</h2>
             <div class="precios">
-              <button class="precio-valor">${data[main].Precio}</button>
-              <button class="Btn"></button>
-                <div class="container">
-                    <div class="btn"><a href="#">Hacerlo solo</a></div>
-                </div>
-            </div>
+        <div class="caja_btn">
+              <button class="btn"><p>${data[main].Precio}</p></button>
+              <button class="btn"><p>COMPRAR</p></button>
+
+  <button class="cta">
+    <span>Hacerlo solo</span>
+    <svg viewBox="0 0 13 10" height="10px" width="15px">
+      <path d="M1,5 L11,5"></path>
+      <polyline points="8 1 12 5 8 9"></polyline>
+    </svg>
+  </button>
+
+        </div>
           </section>
 
         
